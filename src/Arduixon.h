7db8375ff0ffda0249d44fb2ea7a2b1d967b1458
Wolfyxon <wolfyxon@gmail.com>
2025-11-08 22:35:30 +0100

    Sometimes the LED_BUILTIN def gets overriden

--- src/Arduixon.h
+++ src/Arduixon.h
@@ -43,14 +43,13 @@ ulong millis_to_micros(ulong millis);
 
 /* Polyfills */
 
-#ifndef LED_BUILTIN
-    #ifdef CONFIG_IDF_TARGET_ESP32
-        #define LED_BUILTIN 2
-    #elif CONFIG_IDF_TARGET_ESP32S2
-        #define LED_BUILTIN 15
-    #else
-        #warning 'LED_BUILTIN' not defined for this board
-    #endif
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+    #define LED_BUILTIN 2
+#elif CONFIG_IDF_TARGET_ESP32S2
+    #define LED_BUILTIN 15
+#else
+    #warning 'LED_BUILTIN' not defined for this board
 #endif
 
 #endif
